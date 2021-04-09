f2c6f1f55ad2d50ca7865b204792c3d84892e78b
mhennis7 <80371846+mhennis7@users.noreply.github.com>
2021-04-08 19:18:50 -0700

    2nd time

---
+++ githuhupload/VSCode_Staging/app.py
@@ -0,0 +1,9 @@
+def num_range():
+    for x in range(10):
+        print(10)
+
+num_range()     
+
+
+
+
--- githuhupload/main.py
+++ githuhupload/main.py
@@ -3,3 +3,8 @@ def main():
 
 main()
 
+def num_range():
+    for x in range(10):
+        print(10)
+
+num_range()     
